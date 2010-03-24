1d128085cfdc528126d8fd5883498ca2d7b45539
Martin Foot <martin@mfoot.com>
2010-03-24 20:20:18 +0000

    Moved some functionality into the main window for better coherence.

--- sched.py
+++ sched.py
@@ -24,26 +24,12 @@ if __name__ == "__main__":
 		parser = ModuleXMLParser.ModuleXMLParser()
 		modules = parser.parse(xml)
 		from forms import Ui_ModuleListWidget
-		window.ui.mappingsTable.setSortingEnabled(False)
-		
-		window.ui.mappingsTable.setRowCount(sum([len(x.parameters) for x in modules]) +1)
 		for module in modules:
 			ui = Ui_ModuleListWidget.Ui_ModuleListWidget(module,parent = window.ui.listWidget)
 			window.ui.listWidget.addItem(ui)
-#		window.setCentralWidget(Ui_ModuleListWidget.Ui_ModuleListWidget())
-			counter = 0
-			for param in module.parameters:
-				print "param found" , param.value, param.id, param.description
-	#			ui = Ui_ModuleListWidget.Ui_ModuleListWidget(param.value)
-				window.ui.mappingsTable.setItem(counter, 0, QTableWidgetItem(QString(param.id)))
-				window.ui.mappingsTable.setItem(counter, 1, QTableWidgetItem(QString(param.description)))
-				window.ui.mappingsTable.setItem(counter, 2, QTableWidgetItem(QString(param.value)))
-				counter += 1
-	#			ui = ModuleListWidgetItem.ModuleListWidgetItem()
-	#			ui.setupUi(widget)
-	#			widget.show()
-	#			window.ui.listWidget.addItem(ui)
-		window.ui.mappingsTable.setSortingEnabled(True)
+			
+		window.updateMappings()
+
 	except OSError:
 		print "Warning: ~/.sched doesn't exist. Attempting to create it."
 		try:
