ff6a7dda52aa20fabd39fc0026cb67d9b5a86ff1
Martin Foot <martin@mfoot.com>
2010-03-29 18:53:17 +0100

    Import of two new forms, one for showing job execution progress and one for adding a new module to a project.

--- forms/Ui_NewModuleWizard.py
+++ forms/Ui_NewModuleWizard.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'newmodulewizard.ui'
 #
-# Created: Wed Mar 24 17:52:55 2010
+# Created: Mon Mar 29 18:22:02 2010
 #      by: PyQt4 UI code generator 4.6
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,9 +12,9 @@ from PyQt4 import QtCore, QtGui
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(399, 300)
+        Dialog.resize(399, 354)
         self.okCancelButtons = QtGui.QDialogButtonBox(Dialog)
-        self.okCancelButtons.setGeometry(QtCore.QRect(50, 260, 341, 32))
+        self.okCancelButtons.setGeometry(QtCore.QRect(50, 290, 341, 32))
         self.okCancelButtons.setOrientation(QtCore.Qt.Horizontal)
         self.okCancelButtons.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.okCancelButtons.setObjectName("okCancelButtons")
@@ -22,31 +22,31 @@ class Ui_Dialog(object):
         self.label.setGeometry(QtCore.QRect(10, 10, 351, 16))
         self.label.setObjectName("label")
         self.label_5 = QtGui.QLabel(Dialog)
-        self.label_5.setGeometry(QtCore.QRect(10, 100, 71, 16))
+        self.label_5.setGeometry(QtCore.QRect(10, 130, 71, 16))
         self.label_5.setObjectName("label_5")
         self.globalScopeRadioButton = QtGui.QRadioButton(Dialog)
-        self.globalScopeRadioButton.setGeometry(QtCore.QRect(190, 230, 93, 20))
+        self.globalScopeRadioButton.setGeometry(QtCore.QRect(190, 260, 93, 20))
         self.globalScopeRadioButton.setObjectName("globalScopeRadioButton")
         self.newParameterButton = QtGui.QPushButton(Dialog)
-        self.newParameterButton.setGeometry(QtCore.QRect(310, 120, 81, 27))
+        self.newParameterButton.setGeometry(QtCore.QRect(310, 150, 81, 27))
         self.newParameterButton.setObjectName("newParameterButton")
         self.projectScopeRadioButton = QtGui.QRadioButton(Dialog)
-        self.projectScopeRadioButton.setGeometry(QtCore.QRect(90, 230, 93, 20))
+        self.projectScopeRadioButton.setGeometry(QtCore.QRect(90, 260, 93, 20))
         self.projectScopeRadioButton.setObjectName("projectScopeRadioButton")
         self.label_3 = QtGui.QLabel(Dialog)
         self.label_3.setGeometry(QtCore.QRect(10, 40, 52, 15))
         self.label_3.setObjectName("label_3")
         self.removeParameterButton = QtGui.QPushButton(Dialog)
-        self.removeParameterButton.setGeometry(QtCore.QRect(310, 150, 81, 27))
+        self.removeParameterButton.setGeometry(QtCore.QRect(310, 180, 81, 27))
         self.removeParameterButton.setObjectName("removeParameterButton")
         self.label_2 = QtGui.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(10, 220, 71, 31))
+        self.label_2.setGeometry(QtCore.QRect(10, 250, 71, 31))
         self.label_2.setObjectName("label_2")
         self.nameInputBox = QtGui.QLineEdit(Dialog)
         self.nameInputBox.setGeometry(QtCore.QRect(90, 40, 301, 23))
         self.nameInputBox.setObjectName("nameInputBox")
         self.parameterList = QtGui.QTableWidget(Dialog)
-        self.parameterList.setGeometry(QtCore.QRect(10, 120, 291, 91))
+        self.parameterList.setGeometry(QtCore.QRect(10, 150, 291, 91))
         self.parameterList.setColumnCount(3)
         self.parameterList.setObjectName("parameterList")
         self.parameterList.setColumnCount(3)
@@ -64,6 +64,12 @@ class Ui_Dialog(object):
         self.commandInputBox = QtGui.QLineEdit(Dialog)
         self.commandInputBox.setGeometry(QtCore.QRect(90, 70, 301, 23))
         self.commandInputBox.setObjectName("commandInputBox")
+        self.label_6 = QtGui.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(10, 100, 71, 16))
+        self.label_6.setObjectName("label_6")
+        self.descriptionInputBox = QtGui.QLineEdit(Dialog)
+        self.descriptionInputBox.setGeometry(QtCore.QRect(90, 100, 301, 23))
+        self.descriptionInputBox.setObjectName("descriptionInputBox")
 
         self.retranslateUi(Dialog)
         QtCore.QObject.connect(self.okCancelButtons, QtCore.SIGNAL("accepted()"), Dialog.accept)
@@ -84,4 +90,5 @@ class Ui_Dialog(object):
         self.parameterList.horizontalHeaderItem(1).setText(QtGui.QApplication.translate("Dialog", "Description", None, QtGui.QApplication.UnicodeUTF8))
         self.parameterList.horizontalHeaderItem(2).setText(QtGui.QApplication.translate("Dialog", "Default", None, QtGui.QApplication.UnicodeUTF8))
         self.label_4.setText(QtGui.QApplication.translate("Dialog", "Command:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_6.setText(QtGui.QApplication.translate("Dialog", "Description:", None, QtGui.QApplication.UnicodeUTF8))
 
