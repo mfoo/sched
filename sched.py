72c4dd004406bf4478968f1a0dda567ad696734b
Martin Foot <martin@mfoot.com>
2010-03-31 20:41:59 +0100

    Shortened sched.py and put the main window in control of loading properly

--- sched.py
+++ sched.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
 import sys
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-from forms import MainWindow
+from PyQt4.QtGui import QApplication
+from forms.mainwindow import MainWindow
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	window = MainWindow.MainWindow()
+	window = MainWindow()
 	window.show()
-	window.loadGlobals()
 	sys.exit(app.exec_())
-
